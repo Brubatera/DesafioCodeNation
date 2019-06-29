fd8b2329d79053650b9a1c5f1f7f53d3ff629962
Brubatera <brubatera5@gmail.com>
2019-06-29 11:00:46 -0300

    Desafio CodeNation Java

---
+++ src/ResumoCript.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Executable;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+public class ResumoCript {
+
+    private String resumo = null;
+
+    public String geraResumo(String decifrado) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            digest.reset();
+            digest.update(decifrado.getBytes("utf8"));
+            resumo = String.format("%040x", new BigInteger(1, digest.digest()));
+
+            System.out.println(resumo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resumo;
+    }
+
+}
